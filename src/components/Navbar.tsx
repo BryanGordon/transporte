bddd9bf8c27ff930cbf1d0e22dd6f1d60f7676f8
Bryan Gordon <bryanbge92@gmail.com>
2024-08-15 18:31:29 -0500

    Update navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+export function Navbar () {
+  return (
+    <nav>
+      <ul>
+        <li className='nav-items'>
+          Home
+        </li>
+        <li className='nav-items'>
+          Servicios
+        </li>
+        <li className='nav-items'>
+          Contactos
+        </li>
+        <li className='nav-items'>
+          Ubicación
+        </li>
+      </ul>
+    </nav>
+  )
+}
