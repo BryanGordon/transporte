985e32cf97cf7f993dbe1703db4773267fd77263
Bryan Gordon <bryanbge92@gmail.com>
2024-08-15 18:28:12 -0500

    Update header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { ImgHeader } from './ImgHeader'
+import { Navbar } from './Navbar'
+
+export function Header () {
+  return (
+    <header>
+      <ImgHeader />
+      <Navbar />
+    </header>
+  )
+}
