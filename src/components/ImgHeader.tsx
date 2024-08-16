8b34448bd9ab96b2ea5fdacd5c47d26f4a2472b4
Bryan Gordon <bryanbge92@gmail.com>
2024-08-16 18:21:58 -0500

    Update component

--- src/components/ImgHeader.tsx
+++ src/components/ImgHeader.tsx
@@ -1,5 +1,8 @@
+import '../ImgStyles.css'
+import Backgroundheader from '../assets/transport-4.jpg'
+
 export function ImgHeader () {
   return (
-    <img src='' alt='' />
+    <img className='img-header' alt='transport-img' />
   )
 }
