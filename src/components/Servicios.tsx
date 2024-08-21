ea25ef829318b43225996b8ef9b85630c886ac23
Bryan Gordon <bryanbge92@gmail.com>
2024-08-20 19:38:59 -0500

    Update footer

--- src/components/Servicios.tsx
+++ src/components/Servicios.tsx
@@ -1,7 +1,23 @@
+import '../Servicios.css'
+
 export function Servicios () {
   return (
     <article>
-      Servicios
+      <h1>Ofertamos diferentes tipos de servicios</h1>
+      <span>Colocar carrousel</span>
+
+      <h2>Servicios de transporte de mercancias</h2>
+      <img src='' alt='' />
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, molestias.</p>
+
+      <h2>Servicios de logistica</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, dolores?</p>
+      <img src='' alt='' />
+
+      <h2>Servicios de carga pesada</h2>
+      <img src='' alt='' />
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt, sed?</p>
+
     </article>
   )
 }
