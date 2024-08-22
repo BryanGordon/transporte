ca6a0aab00a3931ec2fb647f1e23292530b4dca9
Bryan Gordon <bryanbge92@gmail.com>
2024-08-21 19:17:30 -0500

    Update footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,22 @@ import '../Footer.css'
 export function Footer () {
   return (
     <footer>
-      Derechos reservados
+      <div>
+        <span className='text-slate-800 ml-5 mt-4 p-0'>Derechos reservados</span>
+      </div>
+
+      <div className='icons-container'>
+        <a target='_blank' rel='noreferrer' href='https://www.google.com'>
+          <i>icon 1</i>
+        </a>
+        <a target='_blank' rel='noreferrer' href='https://www.google.com'>
+          <i>icon 2</i>
+        </a>
+        <a target='_blank' rel='noreferrer' href='https://www.google.com'>
+          <i>icon 3</i>
+        </a>
+      </div>
+
     </footer>
   )
 }
