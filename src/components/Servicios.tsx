214d084a96b6f2c1603da4ccc19990e9d0e08f15
Bryan Gordon <bryanbge92@gmail.com>
2024-08-22 19:46:55 -0500

    Update servicios

--- src/components/Servicios.tsx
+++ src/components/Servicios.tsx
@@ -1,5 +1,6 @@
 import '../Servicios.css'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel'
+import Img from '../assets/banner-5.jpg'
 
 export function Servicios () {
   return (
@@ -19,24 +20,38 @@ export function Servicios () {
       </div>
 
       <div className='data-container'>
-        <div>
+        <div className='mb-10'>
           <h2>Servicios de transporte de mercancias</h2>
           <div className='data'>
-            <img src='' alt='imagen-1' />
-            <p className='img-1'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, molestias.</p>
+            <img className='data-img' src={Img} alt='imagen-1' />
+            <p className='img-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Molestiae provident sed rerum porro placeat numquam iusto. Itaque deleniti
+              saepe quo assumenda ea perferendis adipisci quibusdam aperiam quas, rerum
+              odit recusandae.
+            </p>
           </div>
         </div>
 
-        <div>
+        <div className='mb-10'>
           <h2>Servicios de logistica</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, dolores?</p>
-          <img src='' alt='imagen-2' />
+          <div className='data'>
+            <p className='img-1'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa reprehenderit
+              velit excepturi laborum, laudantium amet adipisci mollitia numquam, iusto illum
+              debitis inventore necessitatibus minima qui, doloremque unde dolorem eius illo?
+            </p>
+            <img className='data-img' src={Img} alt='imagen-2' />
+          </div>
         </div>
 
-        <div>
+        <div className='mb-10'>
           <h2>Servicios de carga pesada</h2>
-          <img src='' alt='imagen-3' />
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt, sed?</p>
+          <div className='data'>
+            <img className='data-img' src={Img} alt='imagen-3' />
+            <p className='img-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil,
+              ipsam ea! Quisquam officiis quidem fuga necessitatibus. Eveniet officia,
+              architecto sit minus ab velit, vel optio sequi consequuntur ratione nesciunt vero?
+            </p>
+          </div>
         </div>
 
       </div>
