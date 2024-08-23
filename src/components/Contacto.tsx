d7663748dc077e911d2aa4580f9b7c29d60565e5
Bryan Gordon <bryanbge92@gmail.com>
2024-08-23 18:25:28 -0500

    Update contacto

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -1,7 +1,21 @@
+import '../Contacto.css'
+import Img from '../assets/transportes-1.jpg'
+
 export function Contacto () {
   return (
     <article>
       <h1>Contactos</h1>
+      <h2>Sede principal</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempore fugiat
+        illo facere ipsam ntusa. Cupiditate nobis quam nostrum ea alias asperiores aspernatur,
+        deserunt iusto voluptate consequuntur placeat nemo recusandae accusantium,
+        eligendi rerum, iste in. Distinctio magnam labore voluptates voluptatem!
+      </p>
+      <div className='img-container'>
+        <img className='min-w-[450px] min-h-[250px]' src={Img} alt='img-store' />
+      </div>
+
+      <h2>Nuestras redes sociales</h2>
     </article>
   )
 }
