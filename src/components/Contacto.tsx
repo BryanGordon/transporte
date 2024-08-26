d0006ba6b42225a90073fcbff4f58d5c10678630
Bryan Gordon <bryanbge92@gmail.com>
2024-08-26 18:36:16 -0500

    Update component

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -1,5 +1,8 @@
+import { WhatsappIcon } from '@/icons/WhatsappIcon'
 import '../Contacto.css'
 import Img from '../assets/transportes-1.jpg'
+import { InstagramIcon } from '@/icons/InstagramIcon'
+import { FacebookIcon } from '@/icons/FacebookIcon'
 
 export function Contacto () {
   return (
@@ -12,10 +15,23 @@ export function Contacto () {
         eligendi rerum, iste in. Distinctio magnam labore voluptates voluptatem!
       </p>
       <div className='img-container'>
-        <img className='min-w-[450px] min-h-[250px]' src={Img} alt='img-store' />
+        <img className='min-w-[450px] min-h-[250px] rounded-xl' src={Img} alt='img-store' />
       </div>
 
-      <h2>Nuestras redes sociales</h2>
+      <article>
+        <h2>Nuestras redes sociales</h2>
+        <div className='socials-container'>
+          <a href='https://google.com'>
+            <WhatsappIcon />
+          </a>
+          <a href='https://google.com'>
+            <InstagramIcon />
+          </a>
+          <a href='https://google.com'>
+            <FacebookIcon />
+          </a>
+        </div>
+      </article>
     </article>
   )
 }
