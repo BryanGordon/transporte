3a04c74b8a0188a49b892c516900e635ebcabc94
Bryan Gordon <bryanbge92@gmail.com>
2024-08-28 18:43:42 -0500

    Add props

--- src/icons/FacebookIcon.tsx
+++ src/icons/FacebookIcon.tsx
@@ -1,14 +1,20 @@
-export function FacebookIcon () {
+interface Props {
+  width: number
+  height: number
+  strokeWidth: number
+}
+
+export function FacebookIcon ({ width, height, strokeWidth }: Props) {
   return (
     <svg
-      width='55'
-      height='55'
+      width={width}
+      height={height}
       viewBox='0 0 24 24'
       fill='none'
       stroke='#5264E0'
-      stroke-width='1'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth={strokeWidth}
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
       <path d='M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3' />
