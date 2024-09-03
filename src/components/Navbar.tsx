b3e6f13eb411a129e48c31ae4b4200443ee9f738
Bryan Gordon <bryanbge92@gmail.com>
2024-09-03 14:07:33 -0500

    Improve reading structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,28 +3,35 @@ import '../Navbar.css'
 export function Navbar () {
   return (
     <nav>
+
       <ul className='nav-item-container'>
+
         <li className='nav-items'>
           <a href='#home'>
             Home
           </a>
         </li>
+
         <li className='nav-items'>
           <a href='#servicios'>
             Servicios
           </a>
         </li>
+
         <li className='nav-items'>
           <a href='#contactos'>
             Contactos
           </a>
         </li>
+
         <li className='nav-items'>
           <a href='#ubicacion'>
             Ubicación
           </a>
         </li>
+
       </ul>
+
     </nav>
   )
 }
