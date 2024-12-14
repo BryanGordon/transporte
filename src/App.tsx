f96a6081377bd460ba8a80984a28c8775c905ed9
Bryan Gordon <bryanbge92@gmail.com>
2024-12-13 20:06:47 -0500

    Testing framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'motion/react'
+import { AnimatePresence, motion } from 'motion/react'
 
 import { Contacto } from './components/Contacto'
 import { Footer } from './components/Footer'
@@ -6,20 +6,38 @@ import { Header } from './components/Header'
 import { Servicios } from './components/Servicios'
 import { Ubicacion } from './components/Ubicacion'
 
+/**
+ * Arreglar estructura del proyecto
+ * Acabar de colocar las animaciones en la seccion de direccion y
+ * analizar si colocar en la seccion de servicios.
+ */
+
 function App () {
   return (
     <main>
+      <AnimatePresence>
+
       <Header />
       <section>
-        <Servicios />
+      
+          <Servicios />
+
       </section>
       <section>
+         < motion.div 
+          whileInView={{ y: 50 }}
+          viewport={{ amount: 0.3 }}
+          exit={{y: -50}}
+          >
         <Contacto />
+      </motion.div>
       </section>
       <section>
         <Ubicacion />
       </section>
       <Footer />
+      </AnimatePresence>
+
     </main>
   )
 }
