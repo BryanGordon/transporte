e981e593ae96f7f8db15c97a1e27cc6e4a724da9
Bryan Gordon <bryanbge92@gmail.com>
2024-12-19 19:39:40 -0500

    Trying global state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { useStore } from '@/store/useStore'
 import { ImgHeader } from './ImgHeader'
 import { Navbar } from './Navbar'
 
 export function Header () {
+  const value = useStore((state) => state.clicked)
+  console.log('Header state  ' + value)
   return (
     <header id='home'>
       <ImgHeader />
