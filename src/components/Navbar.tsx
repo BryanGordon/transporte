9da2ce9184b73738a13878b3454066d8cca82f68
Bryan Gordon <bryanbge92@gmail.com>
2024-12-20 18:29:27 -0500

    Add global state

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import '../Navbar.css'
+import { useStore } from '@/store/useStore'
 
 export function Navbar () {
+  const { setChangeClick } = useStore()
+
   return (
     <nav>
 
       <ul className='nav-item-container'>
 
         <li className='nav-items'>
-          <a href='#home'>
+          <a href='#home' onClick={() => setChangeClick()}>
             Home
           </a>
         </li>
@@ -19,7 +22,7 @@ export function Navbar () {
         </li>
 
         <li className='nav-items'>
-          <a href='#contactos' onClick={() => console.log('Pulsado')}>
+          <a href='#contactos' onClick={() => setChangeClick()}>
             Contactos
           </a>
         </li>
