a1f3be5e9736296af1112a746b10350be802a970
Bryan Gordon <bryanbge92@gmail.com>
2024-12-19 19:40:19 -0500

    Solve state bug

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -2,5 +2,5 @@ import { create } from 'zustand'
 
 export const useStore = create((set) => ({
   clicked: false,
-  setClicked: set((state) => ({ clicked: state.clicked }))
+  setClicked: () => set((state) => ({ clicked: !state.clicked }))
 }))
