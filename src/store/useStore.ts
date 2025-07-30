6fbe16dfa37ba7e35430cd4565ab34b268bb6e21
Bryan Gordon <bryanbge92@gmail.com>
2025-07-30 18:36:57 -0500

    Delete global state

--- src/store/useStore.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand'
-import { type TriggerMotion } from '@/types/types'
-
-export const useStore = create((set) => ({
-  clicked: false,
-  setChangeClick: () => set((state:TriggerMotion) => ({ clicked: !state.clicked }))
-}))
