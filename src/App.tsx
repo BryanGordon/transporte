a6d1fbcba697fa20089e03d1c7ffd8b308a90917
Bryan Gordon <bryanbge92@gmail.com>
2025-07-30 18:38:24 -0500

    Update animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { motion } from 'motion/react'
+import { motion } from 'motion/react'
 
 import { Contacto } from './components/Contacto'
 import { Footer } from './components/Footer'
@@ -7,29 +7,45 @@ import { Servicios } from './components/Servicios'
 import { Ubicacion } from './components/Ubicacion'
 
 function App () {
-  /*
   const animation = {
     y: {
-      duration: 0.6,
+      duration: 0.5,
       repeat: 0,
       type: 'orchestation'
     }
   }
-*/
   return (
     <main>
       <Header />
-      <section>
+      <motion.section 
+        transition={animation}
+        whileInView={{ 
+          y:['0%', '4%', '0%']
+        }}
+        viewport={{once: true}}
+      >
         <Servicios />
-      </section>
+      </motion.section>
 
-      <section>
+      <motion.section 
+        transition={animation}
+        whileInView={{ 
+          y:['0%', '4%', '0%']
+        }}
+        viewport={{once: true}}
+      >
         <Contacto />
-      </section>
+      </motion.section>
 
-      <section>
+      <motion.section
+      transition={animation}
+      whileInView={{
+        y: ['0%', '4%', '0%']
+      }}
+      viewport={{once: true}}
+      >
         <Ubicacion />
-      </section>
+      </motion.section>
       <Footer />
 
     </main>
